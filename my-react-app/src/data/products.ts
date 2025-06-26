a8495e8ff0242673e4a971351676c145f4d2c193
Abdul Rahman <ar9003723453@gmail.com>
2025-06-26 16:54:57 +0530

    add one product

--- my-react-app/src/data/products.ts
+++ my-react-app/src/data/products.ts
@@ -45,5 +45,16 @@ export const products: Product[] = [
       { id: 'mount-road', name: 'Mount Road Stock', quantity: 8 },
       { id: 'office', name: 'Office Stock', quantity: 20 }
     ]
+  },
+  {
+    id: '4',
+    name: 'Sony PlayStation 5 Console Ps5 Pro White 2 TB',
+    rate: '50999',
+    image: 'https://micromini.in/wp-content/uploads/2024/12/GUEST_221282a8-da0b-415b-8668-91ac57594067.webp',
+    stocks: [
+      { id: 'tambaram', name: 'Tambaram Stock', quantity: 10 },
+      { id: 'mount-road', name: 'Mount Road Stock', quantity: 10 },
+      { id: 'office', name: 'Office Stock', quantity: 20 }
+    ]
   }
 ];
